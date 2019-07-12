b592f6bcfb3af4e8a0b1d3d797dcba2cbcebb095
n_jansen <nico.jansen1@rwth-aachen.de>
2019-07-12 12:31:57 +0200

    Add language for parsing JSON artifacts.

---
+++ src/test/java/json/parser/JSONParserTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017, MontiCore. All rights reserved. http://www.se-rwth.de/
+ */
+package json.parser;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import org.antlr.v4.runtime.RecognitionException;
+import org.junit.Test;
+
+import json._ast.ASTJSONDocument;
+import json._parser.JSONParser;
+
+public class JSONParserTest {
+  
+  @Test
+  public void testState() throws RecognitionException, IOException {
+    Path model = Paths.get("src/test/resources/json/parser/bookstore.json");
+    JSONParser parser = new JSONParser();
+    
+    Optional<ASTJSONDocument> jsonDoc = parser.parse(model.toString());
+    assertFalse(parser.hasErrors());
+    assertTrue(jsonDoc.isPresent());
+  }
+}
