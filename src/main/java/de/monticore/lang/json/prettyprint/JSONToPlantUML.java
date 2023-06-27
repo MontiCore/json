7c4efce5abf869960f50bacbc631bd4a095decaa
Philipp Wynands <philippwynands@gmx.de>
2023-06-27 14:23:10 +0200

    ADDED: JSONToPlantUML Pretty Printer like Marc would like to have it (Philipp Wynands)

--- src/main/java/de/monticore/lang/json/prettyprint/JSONToPlantUML.java
+++ src/main/java/de/monticore/lang/json/prettyprint/JSONToPlantUML.java
@@ -1,15 +1,39 @@
 package de.monticore.lang.json.prettyprint;
 
-import de.monticore.lang.json._ast.ASTJSONDocument;
+import de.monticore.ast.ASTNode;
+import de.monticore.lang.json.JSONMill;
+import de.monticore.lang.json._ast.*;
 import de.monticore.lang.json._prettyprint.JSONFullPrettyPrinter;
+import de.monticore.lang.json._visitor.JSONHandler;
+import de.monticore.lang.json._visitor.JSONTraverser;
+import de.monticore.lang.json._visitor.JSONVisitor2;
+import de.monticore.literals.mccommonliterals._ast.ASTSignedBasicFloatLiteral;
+import de.monticore.literals.mccommonliterals._ast.ASTSignedBasicLongLiteral;
+import de.monticore.literals.mccommonliterals._ast.ASTSignedNatLiteral;
+import de.monticore.literals.mccommonliterals._ast.ASTStringLiteral;
+import de.monticore.literals.mccommonliterals._visitor.MCCommonLiteralsVisitor2;
 import de.monticore.prettyprint.IndentPrinter;
+import de.monticore.symboltable.IScope;
+import de.monticore.symboltable.ISymbol;
 
-public class JSONToPlantUML {
-
-    JSONFullPrettyPrinter prettyPrinter;
+public class JSONToPlantUML implements JSONVisitor2, MCCommonLiteralsVisitor2, JSONHandler {
+    private IndentPrinter printer;
+    private JSONTraverser traverser;
 
     public JSONToPlantUML() {
-        this.prettyPrinter = new JSONFullPrettyPrinter(new IndentPrinter(), false);
+        this.printer = new IndentPrinter();
+        this.traverser = JSONMill.traverser();
+        this.traverser.add4JSON(this);
+        this.traverser.add4MCCommonLiterals(this);
+        this.traverser.setJSONHandler(this);
+    }
+
+    public JSONTraverser getTraverser() {
+        return this.traverser;
+    }
+
+    public void setTraverser(JSONTraverser traverser) {
+        this.traverser = traverser;
     }
 
     /**
@@ -19,9 +43,122 @@ public class JSONToPlantUML {
      * @return The pretty-printed JSON-AST as String
      */
     public String printJSONDocument(ASTJSONDocument jsonDocument) {
-        String printedModel = "@startjson\n" + this.prettyPrinter.prettyprint(jsonDocument) + "\n@endjson";
-        return printedModel;
+        this.printer.clearBuffer();
+        this.getTraverser().handle(jsonDocument);
+        String puml = printer.getContent();
+        return puml;
     }
+
+    @Override
+    public void handle(ASTJSONObject node) {
+        this.getTraverser().visit(node);
+        int length = node.getPropList().size();
+        for (int i = 0; i < length; i++) {
+            node.getProp(i).accept(this.getTraverser());
+            if (i < length - 1) {
+                this.printer.println(",");
+            } else {
+                this.printer.println();
+            }
+        }
+        this.getTraverser().endVisit(node);
+    }
+
+    @Override
+    public void handle(ASTJSONArray node) {
+        this.getTraverser().visit(node);
+        int length = node.getJSONValueList().size();
+        for (int i = 0; i < length; i++) {
+            node.getJSONValue(i).accept(this.getTraverser());
+            if (i < length - 1) {
+                this.printer.println(",");
+            } else {
+                this.printer.println();
+            }
+        }
+        this.getTraverser().endVisit(node);
+    }
+
+    @Override
+    public void visit(ASTJSONDocument node) {
+        this.printer.println("@startjson");
+    }
+
+    @Override
+    public void endVisit(ASTJSONDocument node) {
+        this.printer.println();
+        this.printer.print("@endjson");
+    }
+
+    @Override
+    public void visit(ASTJSONObject node) {
+        this.printer.println("{");
+        this.printer.indent();
+    }
+
+    @Override
+    public void endVisit(ASTJSONObject node) {
+        this.printer.unindent();
+        this.printer.print("}");
+    }
+
+    @Override
+    public void visit(ASTJSONArray node) {
+        this.printer.println("[");
+        this.printer.indent();
+    }
+
+    @Override
+    public void endVisit(ASTJSONArray node) {
+        this.printer.unindent();
+        this.printer.print("]");
+    }
+
+    @Override
+    public void visit(ASTJSONBoolean node) {
+        this.printer.print(node.getBooleanLiteral().getValue());
+    }
+
+    @Override
+    public void visit(ASTJSONNull node) {
+        this.printer.print("null");
+    }
+
+    @Override
+    public void visit(ASTJSONProperty node) {
+        this.printer.print("\"" + node.getKey() + "\": ");
+    }
+
+    @Override
+    public void endVisit(ASTJSONProperty node) {
+
+    }
+
+    @Override
+    public void visit(ASTSignedBasicDoubleLiteral node) {
+        this.printer.print(node.getSource());
+    }
+
+    @Override
+    public void visit(ASTSignedBasicFloatLiteral node) {
+        this.printer.print(node.getSource());
+    }
+
+    @Override
+    public void visit(ASTSignedBasicLongLiteral node) {
+        this.printer.print(node.getSource());
+    }
+
+    @Override
+    public void visit(ASTSignedNatLiteral node) {
+        this.printer.print(node.getSource());
+    }
+
+    @Override
+    public void visit(ASTStringLiteral node) {
+        this.printer.print("\"" + node.getSource() + "\"");
+    }
+
 }
 
 
