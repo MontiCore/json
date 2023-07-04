4a197e8ff1a64a50536d6df19e65af0dccd76748
Philipp Wynands <philippwynands@gmx.de>
2023-07-04 15:57:30 +0200

    ADDED: PlantUMLUtil.java

---
+++ src/main/java/de/monticore/lang/json/prettyprint/PlantUMLUtil.java
@@ -0,0 +1,121 @@
+package de.monticore.lang.json.prettyprint;
+
+import de.monticore.lang.json._ast.ASTJSONDocument;
+import de.monticore.lang.json._parser.JSONParser;
+import de.se_rwth.commons.logging.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
+
+public class PlantUMLUtil {
+    public static final String PLANTUML_EMPTY = "@startuml\n@enduml";
+
+    /**
+     * this needs GraphViz/JDOT installed on your PC
+     */
+    public static Path writeJsonToPlantUmlSvg(
+            @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<ASTJSONDocument> astJSON,
+            Path outputPathSVG,
+            PlantUMLConfig plantUMLConfig)
+            throws IOException {
+
+        final String plantUMLString = toPlantUmlModelString(astJSON, plantUMLConfig);
+        final SourceStringReader reader = new SourceStringReader(plantUMLString);
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // Write the first image to "os"
+        reader.outputImage(os, new FileFormatOption(FileFormat.SVG));
+        os.close();
+
+        // The XML is stored into svg
+        final String svg = new String(os.toByteArray(), StandardCharsets.UTF_8);
+        try (PrintWriter out = new PrintWriter(outputPathSVG.toString())) {
+            out.println(svg);
+        }
+
+        return outputPathSVG;
+    }
+
+
+    /**
+     * this needs GraphViz/JDOT installed on your PC
+     */
+    public static void writeJsonToPlantUmlSvg(
+            String pathJSON, Path outputPathSVG, PlantUMLConfig plantUMLConfig) throws IOException {
+
+        final String cdString = new String(Files.readAllBytes(Paths.get(pathJSON)));
+
+        final String plantUMLString = toPlantUmlModelString(cdString, plantUMLConfig);
+        final SourceStringReader reader = new SourceStringReader(plantUMLString);
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // Write the first image to "os"
+        reader.outputImage(os, new FileFormatOption(FileFormat.SVG));
+        os.close();
+
+        // The XML is stored into svg
+        final String svg = new String(os.toByteArray(), StandardCharsets.UTF_8);
+        try (PrintWriter out = new PrintWriter(outputPathSVG.toString())) {
+            out.println(svg);
+        }
+    }
+
+    public static Path writeCdToPlantUmlModelFile(
+            @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<ASTJSONDocument> astJSON,
+            Path outputPath,
+            PlantUMLConfig plantUMLConfig)
+            throws IOException {
+        final String plantUMLString = toPlantUmlModelString(astJSON, plantUMLConfig);
+
+        try (PrintWriter out = new PrintWriter(outputPath.toString())) {
+            out.println(plantUMLString);
+        }
+
+        return outputPath;
+    }
+
+    public static void writeCdToPlantUmlModelFile(
+            String pathJSON, Path outputPath, PlantUMLConfig plantUMLConfig) throws IOException {
+        final String cdString = new String(Files.readAllBytes(Paths.get(pathJSON)));
+
+        final String plantUMLString = toPlantUmlModelString(cdString, plantUMLConfig);
+
+        try (PrintWriter out = new PrintWriter(outputPath.toString())) {
+            out.println(plantUMLString);
+        }
+    }
+
+    protected static String toPlantUmlModelString(
+            @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<ASTJSONDocument> astJSON,
+            PlantUMLConfig config) {
+        JSONToPlantUML prettyPrinter = new JSONToPlantUML(config);
+
+        if (astJSON.isPresent()) {
+            return prettyPrinter.printJSONDocument(astJSON.get());
+        }
+
+        return PLANTUML_EMPTY;
+    }
+
+    protected static String toPlantUmlModelString(String jsonString, PlantUMLConfig config) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            Optional<ASTJSONDocument> astJSON = parser.parse_String(jsonString);
+            return toPlantUmlModelString(astJSON, config);
+        } catch (IOException e) {
+            Log.error("Cannot display CD since it contains errors!");
+        }
+
+        return PLANTUML_EMPTY;
+    }
+}
+
